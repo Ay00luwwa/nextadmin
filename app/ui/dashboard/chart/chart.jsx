f1a86fc0f72c9ba70e43d9fc48d771f50b0436ce
Ay00luwwa <ijidakinroayooluwa@gmail.com>
2023-12-02 21:13:45 +0100

    hhhhh

---
+++ app/ui/dashboard/chart/chart.jsx
@@ -0,0 +1,7 @@
+import styles from "./chart.module.css";
+
+const Chart = () => {
+  return <div></div>;
+};
+
+export default Chart;
---
+++ app/ui/dashboard/transactions/transactions.jsx
@@ -0,0 +1,7 @@
+import styles from "./transactions.module.css";
+
+const Transactions = () => {
+  return <div>transactions</div>;
+};
+
+export default Transactions;
