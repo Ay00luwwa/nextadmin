b26b8cf4db41f8e4bac096ac80774dc642466c59
Ay00luwwa <ijidakinroayooluwa@gmail.com>
2024-01-02 00:46:19 +0100

    update

--- app/dashboard/users/[id]/page.jsx
+++ app/dashboard/users/[id]/page.jsx
@@ -5,37 +5,43 @@ import Image from "next/image";
 const SingleUserPage = () => {
   return (
     <div className={styles.container}>
-      <div className={infoContainer}>
+      <div className={styles.infoContainer}>
         <div className={styles.imageContainer}>
           <Image src="/default.jpg" alt="" fill />
         </div>
         Philip Plein
       </div>
-      <div className={formContainer}>
-        <label>Username</label>
-        <input type="text" placeholder="Philip Plein" name="username" />
-        <label>Email</label>
-        <input type="email" placeholder="PhilipPlein@email.com" name="email" />
-        <label>Password</label>
-        <input type="password" placeholder="Philip Plein" name="password" />
-        <label>Phone</label>
-        <input type="text" placeholder="+123456789" name="phone" />
-        <label>Address</label>
-        <textarea
-          type="text"
-          placeholder="22 MapleStreet, New York"
-          name="address"
-        />
-        <label>Is Admin?</label>
-        <select name="isAdmin" id="isAdmin">
-          <option value={true}>Yes</option>
-          <option value={false}>No</option>
-        </select>
-        <label>Is Active?</label>
-        <select name="isActive" id="isActive">
-          <option value={true}>Yes</option>
-          <option value={false}>No</option>
-        </select>
+      <div className={styles.formContainer}>
+        <form action="" className={styles.form}>
+          <label>Username</label>
+          <input type="text" placeholder="Philip Plein" name="username" />
+          <label>Email</label>
+          <input
+            type="email"
+            placeholder="PhilipPlein@email.com"
+            name="email"
+          />
+          <label>Password</label>
+          <input type="password" placeholder="" name="password" />
+          <label>Phone</label>
+          <input type="text" placeholder="+123456789" name="phone" />
+          <label>Address</label>
+          <textarea
+            type="text"
+            placeholder="22 MapleStreet, New York"
+            name="address"
+          />
+          <label>Is Admin?</label>
+          <select name="isAdmin" id="isAdmin">
+            <option value={true}>Yes</option>
+            <option value={false}>No</option>
+          </select>
+          <label>Is Active?</label>
+          <select name="isActive" id="isActive">
+            <option value={true}>Yes</option>
+            <option value={false}>No</option>
+          </select>
+        </form>
       </div>
     </div>
   );
